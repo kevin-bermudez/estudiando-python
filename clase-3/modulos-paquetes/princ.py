5042789a1291df3eafe8f86f027e1e102a432ef5
kevin-bermudez <kevinbermudezmejia@gmail.com>
2024-02-17 00:00:44 -0500

    feat: add resources for class 3

---
+++ clase-3/modulos-paquetes/princ.py
@@ -0,0 +1,6 @@
+import mimodulo
+
+resultado = mimodulo.sumar(7, 5)
+
+print(resultado)
+print(mimodulo.pi)
---
+++ clase-3/modulos-paquetes/princparcial.py
@@ -0,0 +1,6 @@
+from mimodulo import sumar,pi
+
+resultado = sumar(7, 5)
+
+print(resultado)
+print(pi)
